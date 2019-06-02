789d40dde78ef9976796ee2ac472a7a9684db812
Oksana Rudkevych <aveerudkevich@gmail.com>
2019-06-02 14:51:55 +0300

    final task 6.1

--- plants/controller.js
+++ plants/controller.js
@@ -1,6 +1,6 @@
 let plants = [haworthia, philodendron, echeveria, peperomia, haworthiaZebra, pilea, snake, succulentTrio];
 
-let tmp = function tmp() {
+let changeColor = function changeColor() {
     let buttonColor = this.getAttribute('class'); // получаем класс кнопки - это цвет green/red
     let buttonDiv = this.parentNode; // достаем див в котором лежит кнопка
     let divImage = buttonDiv.firstChild; // достаем p из div`а
@@ -17,14 +17,15 @@ function createPlantCard(plant) {
     let cardsWrapper = document.getElementById('cardsWrapper');
     let divForProduct = document.createElement('div');
 
-    let plantImage = document.createElement('img');
-    plantImage.src = plant.productImage;
-    plantImage.id = 'divImage';
-    divForProduct.appendChild(plantImage);
-
     divForProduct.className = 'card';
     divForProduct.id = plant.productName;
     cardsWrapper.appendChild(divForProduct);
+
+    let plantImage = document.createElement('img');
+    plantImage.className = 'plantImage';
+    plantImage.src = plant.productImage;
+    plantImage.id = 'divImage';
+    divForProduct.appendChild(plantImage);
 
     let fullPlantName = document.createElement('h4');
     fullPlantName.innerHTML = plant.fullProductName;
@@ -42,17 +43,69 @@ function createPlantCard(plant) {
     divForProduct.appendChild(plantPrice);
 
     let greenButton = document.createElement('button');
-    greenButton.onclick = tmp;
+    greenButton.onclick = changeColor;
     greenButton.className = 'green';
     divForProduct.appendChild(greenButton);
 
     let redButton = document.createElement('button');
-    redButton.onclick = tmp;
+    redButton.onclick = changeColor;
     redButton.className = 'red';
     divForProduct.appendChild(redButton);
 
     let whiteButton = document.createElement('button');
-    whiteButton.onclick = tmp;
+    whiteButton.onclick = changeColor;
     whiteButton.className = 'white';
     divForProduct.appendChild(whiteButton);
+
+    // добавить кнопку купить
+}
+
+// переименовать
+function switchToList() {
+
+    let cards = document.getElementsByClassName('card');
+    for (i = 0; i < cards.length; i++) {
+        cards[i].classList.toggle("cardList");
+    }
+
+    let plantImages = document.getElementsByClassName('plantImage');
+    for (i = 0; i < plantImages.length; i++) {
+        plantImages[i].classList.toggle("plantImageList");
+    }
+
+    let fullPlantNames = document.getElementsByClassName('fullPlantName');
+    for (i = 0; i < fullPlantNames.length; i++) {
+        fullPlantNames[i].classList.toggle("fullPlantNameList");
+    }
+
+    let plantSizes = document.getElementsByClassName('plantSize');
+    for (i = 0; i < plantSizes.length; i++) {
+        plantSizes[i].classList.toggle("plantSizeList");
+    }
+
+    let plantPrice = document.getElementsByClassName('plantPrice');
+    for (i = 0; i < plantPrice.length; i++) {
+        plantPrice[i].classList.toggle("plantPriceList");
+    }
+
+    let whiteButtons = document.getElementsByClassName('white');
+    for (i = 0; i < whiteButtons.length; i++) {
+        whiteButtons[i].classList.toggle("whiteButtonList");
+    }
+
+    let redButtons = document.getElementsByClassName('red');
+    for (i = 0; i < redButtons.length; i++) {
+        redButtons[i].classList.toggle("redButtonList");
+    }
+
+    let greenButtons = document.getElementsByClassName('green');
+    for (i = 0; i < greenButtons.length; i++) {
+        greenButtons[i].classList.toggle("greenButtonList");
+    }
+
+    // let buyButtons = document.getElementsByClassName('buyButton');
+    // for (i = 0; i < greenButtons.length; i++) {
+    //     buyButtons[i].classList.toggle("buyButtonList");
+    // }
+
 }
--- plants/model.js
+++ plants/model.js
@@ -1,5 +1,5 @@
 class Plant {
-    constructor(productName, productImage,fullProductName, plantSize, plantPrice) {
+    constructor(productName, productImage, fullProductName, plantSize, plantPrice) {
         this.productName = productName;
         this.productImage = productImage;
         this.fullProductName = fullProductName;
@@ -9,14 +9,13 @@ class Plant {
 }
 
 let haworthia = new Plant('haworthia', 'images/haworthia_green.jpg', 'Haworthia Zebra In Mini Grant Planter', 'Medium', '$30');
-let philodendron = new Plant('philodendron', 'images/philodendron_green.jpg', 'Philodendron Green In Small Grant Planter', 'Small', '23');
+let philodendron = new Plant('philodendron', 'images/philodendron_green.jpg', 'Philodendron Green In Small Grant Planter', 'Small', '$23');
 let echeveria = new Plant('echeveria', 'images/echeveria_green.jpg', 'Echeveria In Mini Dolores Planter', 'Mini', '$38');
 let peperomia = new Plant('peperomia', 'images/peperomia_green.jpg', 'Peperomia Marble In Small Dolores Planter', 'Small', '$30');
 let haworthiaZebra = new Plant('haworthiaZebra', 'images/haworthiaZebra_green.jpg', 'Haworthia Zebra', 'Small', '$54');
 let pilea = new Plant('pilea', 'images/pilea_green.jpg', 'Pileo Calathea Rattlesnake In August Planter', 'Small', '$56');
 let snake = new Plant('snake', 'images/snake_green.jpg', 'Snake Plant Zeylanica In Small Dolores Planter', 'Medium', '$30');
 let succulentTrio = new Plant('succulentTrio', 'images/succulentTrio_green.jpg', 'Air Plant Trio In Tillandz Stand Planter', 'Mini', '$78');
-
 
 let productImages = new Map();
 productImages.set('green', new Map([
@@ -49,13 +48,3 @@ productImages.set('white', new Map([
     ['snake', 'images/snake_white.jpg'],
     ['succulentTrio', 'images/succulentTrio_white.jpg']
 ]));
-
-
-/*
-сделать комиты
-+ добавить цвет - белый -
-+ добавить белую кнопку
-+ переименовать переменные
-+ переименовать картинки
-+ вынести стили в css
- */
