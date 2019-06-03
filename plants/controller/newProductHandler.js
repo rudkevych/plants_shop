88534a70cba276ed9c3243a7ff317b31a8d18826
Oksana Rudkevych <aveerudkevich@gmail.com>
2019-06-03 08:38:52 +0300

    final task 6.2.

--- plants/controller/newProductHandler.js
+++ plants/controller/newProductHandler.js
@@ -1,25 +1,63 @@
-let dataURL;
+let input;
+let dataGreenURL;
+let dataRedURL;
+let dataWhiteURL;
 
-let getImageData = function (event) {
-    let input = event.target;
-    let reader = new FileReader();
-    reader.onload = function () {
-        dataURL = reader.result;
-    };
-
-    reader.readAsDataURL(input.files[0]);
-    let div = document.getElementById('newProductWrapper');
+function printInfoAboutPhotoAdding() {
+    let div = document.getElementById('imgText');
     let p = document.createElement('p');
     div.appendChild(p);
     let fileName = input.files[0].name;
     p.innerHTML = `File ${fileName} is chosen`;
+}
+
+let getGreenImageData = function (event) {
+    input = event.target;
+    let reader = new FileReader();
+    reader.onload = function () {
+        dataGreenURL = reader.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+    printInfoAboutPhotoAdding();
 };
+
+let getRedImageData = function (event) {
+    input = event.target;
+    let reader = new FileReader();
+    reader.onload = function () {
+        dataRedURL = reader.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+    printInfoAboutPhotoAdding();
+};
+
+let getWhiteImageData = function (event) {
+    input = event.target;
+    let reader = new FileReader();
+    reader.onload = function () {
+        dataWhiteURL = reader.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+    printInfoAboutPhotoAdding();
+};
+
 
 function addNewProduct() {
     let greenMap = productImages.get('green');
+    let redMap = productImages.get('red');
+    let whiteMap = productImages.get('white');
+
     let newProductName = document.getElementById('newProductName');
     let newProductNameValue = newProductName.value;
-    greenMap.set(newProductNameValue, dataURL);
+
+    greenMap.set(newProductNameValue, dataGreenURL);
+    redMap.set(newProductNameValue, dataRedURL);
+    whiteMap.set(newProductNameValue, dataWhiteURL);
+
+    console.log('green', greenMap, 'red', redMap, 'white', whiteMap);
 
     let newDescription = document.getElementById('productDescription');
     let newDescriptionValue = newDescription.value;
@@ -30,7 +68,6 @@ function addNewProduct() {
     let newProductPrice = document.getElementById('newProductPrice');
     let newProductPriceValue = newProductPrice.value;
 
-
-    let newProduct = new Plant(newProductNameValue, dataURL, newDescriptionValue, newProductSizeValue, newProductPriceValue);
+    let newProduct = new Plant(newProductNameValue, dataGreenURL, newDescriptionValue, newProductSizeValue, newProductPriceValue);
     createPlantCard(newProduct);
 }
